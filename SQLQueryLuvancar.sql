d7defbaf4073089bdcaa52c1738b039dea589bd3
luizhenrique1725 <araujoluiz1725@gmail.com>
2022-08-25 08:36:59 -0300

    criadas algumas procedures no banco de dados.

--- SQLQueryLuvancar.sql
+++ SQLQueryLuvancar.sql
@@ -1,4 +1,4 @@
-
+/*
 use master
 go
 if exists(select 1 from sys.databases where name = 'luvancar')
@@ -9,9 +9,86 @@ end
 go 
 create database luvancar
 go
+*/
 use luvancar
 go
 
+select * from tarefa
+create proc sp_inserirtarefa
+	@Id int output,
+	@Id_Usuario int,
+	@Descricao varchar(250),
+	@Estatus varchar(50)
+as
+	insert into Tarefa(Id,Id_Usuario,Descricao,Estatus)
+	values(@Id,@Id_Usuario,@Descricao,@Estatus)
+go
+
+create proc sp_buscartarefa
+	@Filtro varchar(150)
+as
+	select Id,Id_Usuario,Descricao,Estatus from Tarefa 
+	where Descricao like '%' + @Filtro + '%'
+go
+
+create proc sp_excluirtarefa
+	@Id int
+as
+	delete from Comentario where Id_Tarefa = @Id
+	delete from Tarefa where Id = @Id
+go
+
+create proc sp_inserircomentario
+	@Id int output,
+	@Id_Usuario int,
+	@Id_Tarefa int,
+	@Descricao varchar(250)
+as
+	insert into Comentario(Id,Id_Usuario,Id_Tarefa,Descricao)
+	values(@Id,@Id_Usuario,@Id_Tarefa,@Descricao)
+go
+
+create proc sp_buscarcomentario
+	@Id_Tarefa int
+as
+	select Id,Id_Usuario,Id_Tarefa,Descricao from Comentario 
+	where Id_Tarefa = @Id_Tarefa
+go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 create table cliente
 (
 idcliente int primary key identity(1,1),
@@ -63,3 +140,4 @@ constraint fkcliente_aluguel foreign key(idcliente) references cliente(idcliente
 constraint fkveiculo_aluguel foreign key(idveiculo) references veiculo(idveiculo),
 constraint fkfuncinario_aluguel foreign key(idfuncionario)references funcionario(idfuncionario)
 
+*/
