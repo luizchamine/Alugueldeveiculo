1a2c8e2c1cdd0095e9515a8fbf54f2d4d867a60d
luizhenrique1725 <araujoluiz1725@gmail.com>
2022-08-25 08:04:45 -0300

    criada a classe Model.

---
+++ Model/Comentario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Comentario
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private int id_Usuario;
+
+        public int Id_Usuario
+        {
+            get { return id_Usuario; }
+            set { id_Usuario = value; }
+        }
+        private int id_Tarefa;
+
+        public int Id_Tarefa
+        {
+            get { return id_Tarefa; }
+            set { id_Tarefa = value; }
+        }
+        private string descricao;
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+    }
+}
